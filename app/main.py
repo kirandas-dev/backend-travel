8aa9ef2f63214886bea3559f8309fcc9f63a9105
kirandas-dev <KiranShanker.Das@student.uts.edu.au>
2023-11-03 15:22:57 +1100

    Some Update

--- app/main.py
+++ app/main.py
@@ -28,30 +28,29 @@ def healthcheck():
     return 'Hello there. Welcome! Get some useful insights using our predictives analytics on the American Retail Store!'
 
 
-@app.get('/getairfare/items/', status_code=200)
-#def airfare_predict(startingAirport: str, destinationAirport: str, store_id: str):
-def airfare_predict():
-                # Make predictions based on user inputs
-        input_data = pd.DataFrame({
-            'startingAirport': ['ATL'],
-            'destinationAirport': ['BOS'],
-            'DayOfWeek': [5],  # Example: 5 for Friday
-            'Month': [4],  # Example: 4 for April
-            'Year': [2022],
-            'DepartureHour': [8],  # Example: 8 for 8:00 AM
-            'DepartureMinute': [0],  # Example: 0 for 0 minutes past the hour
-        })
-        input_data['startingAirport'] = loaded_encoders.transform(input_data['startingAirport'])
-        input_data['destinationAirport'] = loaded_encoders.transform(input_data['destinationAirport'])
+@app.get("/predict_airfare")
+def predict_airfare(startingAirport: str, destinationAirport: str, DayOfWeek: int, Month: int, Year: int, DepartureHour: int, DepartureMinute: int):
+    # Create a user input DataFrame
+    input_data = pd.DataFrame({
+        'startingAirport': [startingAirport],
+        'destinationAirport': [destinationAirport],
+        'DayOfWeek': [DayOfWeek],
+        'Month': [Month],
+        'Year': [Year],
+        'DepartureHour': [DepartureHour],
+        'DepartureMinute': [DepartureMinute]
+    })
 
+    # Transform categorical features using the loaded encoders
+    input_data['startingAirport'] = loaded_encoders.transform(input_data['startingAirport'])
+    input_data['destinationAirport'] = loaded_encoders.transform(input_data['destinationAirport'])
 
-        # Make predictions using the loaded model
-        predicte_airfare = model_load['model'].predict(input_data)
-        predicte_airfare = predicte_airfare.tolist()
-       
+    # Make predictions using the loaded model
+    predicted_airfare = model_load['model'].predict(input_data)
+    predicted_airfare = predicted_airfare.tolist()
 
-        # Return the predicted sales as a JSON response
-        return JSONResponse(content={"Predicted Airfare": predicte_airfare})
+    # Return the predicted airfare as a JSON response
+    return JSONResponse(content={"Predicted Airfare": predicted_airfare})
     
 
 if __name__ == "__main__":
